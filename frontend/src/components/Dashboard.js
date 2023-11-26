0e5d1f78509665f4e0ead5aa8dda70b36f99ac4f
Luna Ong <ong.sze.teng@gmail.com>
2023-11-26 19:04:14 +0800

    logic of stats inc/dec

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,69 +8,136 @@ const Dashboard = () => {
   const [name, setName] = useState("");
   const [isEditingName, setIsEditingName] = useState(false);
   const [age, setAge] = useState(0);
+  const [ageUpDuration, setAgeUpDuration] = useState(2000);
   const [health, setHealth] = useState(100);
+  const [healthPenalty, setHealthPenalty] = useState(0);
   const [hunger, setHunger] = useState(0);
+  const [hungerPenalty, setHungerPenalty] = useState(0);
   const [happiness, setHappiness] = useState(100);
+  const [happinessPenaltyMultiplier, setHappinessPenaltyMultiplier] =
+    useState(1);
   const [speed, setSpeed] = useState(1);
   const [isResettingGame, setIsResettingGame] = useState(false);
 
-  // const increaseAge = () => {
-  //   const timeoutID = setTimeout(() => {
-  //     setAge((prevAge) => {
-  //       console.log(prevAge);
-  //       return prevAge + 1;
-  //     });
-  //   }, 2000);
+  const increaseAge = () => {
+    setHealthPenalty((prevPenalty) => {
+      return prevPenalty + 2;
+    });
+    setHungerPenalty((prevPenalty) => {
+      return prevPenalty + 2;
+    });
+    setHappinessPenaltyMultiplier((prevPenalty) => {
+      return prevPenalty + 0.1;
+    });
+    setHealth((prevHealth) => {
+      return prevHealth - healthPenalty;
+    });
+    setHunger((prevHunger) => {
+      return prevHunger + hungerPenalty;
+    });
+    setHappiness((prevHappiness) => {
+      return (
+        prevHappiness - Math.random() * 20 * (1 / happinessPenaltyMultiplier)
+      );
+    });
+    setAge((prevAge) => {
+      return prevAge + 1;
+    });
+  };
 
-  //   age > 9
-  //     ? clearTimeout(timeoutID)
-  //     : // clearTimeout(timeoutID);
-  //     increaseAge();
-  // };
+  switch (health) {
+    case health >= 0 && health < 20:
+      console.log("lowest health");
+      setHappinessPenaltyMultiplier(0.5);
+      break;
+    case health >= 20 && health < 50:
+      console.log("medium health");
+      setHappinessPenaltyMultiplier(0.75);
+      break;
+    case health >= 50 && health <= 100:
+      console.log("highest health");
+      setHappinessPenaltyMultiplier(1);
+      break;
+    default:
+      console.log("this is default ig");
+  }
 
-  // const timeoutID = setTimeout(() => {
-  //   age > 9
-  //     ? onDeath()
-  //     : setAge((prevAge) => {
-  //         console.log(prevAge);
-  //         return prevAge + 1;
-  //       });
-  //   console.log("every 2 sec");
-  // }, 2000);
+  switch (happiness) {
+    case happiness >= 0 && happiness < 20:
+      console.log("lowest happiness");
+      setHungerPenalty(20);
+      break;
+    case happiness >= 20 && happiness < 50:
+      console.log("medium happiness");
+      setHungerPenalty(10);
+      break;
+    case happiness >= 50 && happiness < 100:
+      console.log("highest happiness");
+      setHungerPenalty(0);
+      break;
+    default:
+      console.log("this is default ig");
+  }
+
+  if (happiness > hunger * 2 && happiness >= 50) {
+    console.log("hap and hunger still ideal");
+    setHealthPenalty(0);
+  } else if (happiness > hunger && happiness >= 50) {
+    console.log("hap and hunger are only acceptable");
+    setHealthPenalty(10);
+  } else {
+    console.log("hap and hunger are terrible");
+    setHealthPenalty(20);
+  }
 
   useEffect(() => {
-  const timeoutID = setTimeout(() => {
-    age > 9 ?
-      // ? onDeath()
-    clearTimeout(timeoutID)
-      : setAge((prevAge) => {
-          console.log(prevAge);
-          return prevAge + 1;
-        });
-    console.log("every 2 sec");
-  }, 2000);
+    const timeoutID = setTimeout(() => {
+      age > 9 ? onDeath(timeoutID) : increaseAge();
+    }, ageUpDuration);
   }, [age]);
+
+  useEffect(() => {
+    health <= 0 ? onDeath() : console.log("staying alive :)");
+  }, [health]);
+
+  const onDeath = (timeoutID) => {
+    clearTimeout(timeoutID);
+  };
 
   const onChangeName = (e) => {
     setName(e.target.value);
   };
 
   const onClickClean = () => {
-    console.log("ffffff");
+    console.log("onClickClean");
+    setHealth((prevHealth) => prevHealth - healthPenalty + 15);
   };
 
   const onClickFeed = () => {
-    console.log("ffffff");
+    console.log("onClickFeed");
+    setHunger((prevHunger) => prevHunger + hungerPenalty - 15);
   };
 
   const onClickPlay = () => {
-    console.log("ffffff");
+    console.log("onClickPlay");
+    setHappiness(
+      (prevHappiness) => prevHappiness + 15 * happinessPenaltyMultiplier
+    );
+  };
+
+  const speedGrowth = () => {
+    setAgeUpDuration((prevDuration) => {
+      return prevDuration - 100;
+    });
   };
 
   const onDecSpeed = () => {
-    speed > 1
-      ? setSpeed((prevCount) => prevCount - 1)
-      : alert("You can't go any slower!");
+    if (speed > 1) {
+      setSpeed((prevCount) => prevCount - 1);
+      setAgeUpDuration((prevDuration) => prevDuration - 100);
+    } else {
+      alert("You can't go any slower!");
+    }
   };
 
   const onIncSpeed = () => {
@@ -91,6 +158,16 @@ const Dashboard = () => {
 
   const onClickResetGame = () => {
     setIsResettingGame(true);
+  };
+
+  const checkOverUnder = (value) => {
+    if (value <= 0) {
+      return (value = 0);
+    } else if (value >= 100) {
+      return (value = 100);
+    } else {
+      return value;
+    }
   };
 
   return (
@@ -124,11 +201,11 @@ const Dashboard = () => {
       <div>
         interactions
         <div>clean</div>
-        <Button />
+        <Button onClick={() => onClickClean()} />
         <div>feed</div>
-        <Button />
+        <Button onClick={() => onClickFeed()} />
         <div>play</div>
-        <Button />
+        <Button onClick={() => onClickPlay()} />
       </div>
       <div>
         game control
